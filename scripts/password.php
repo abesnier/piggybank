5d5b43f1208dd7657bebe9dcdb2db6e9c0ec6209
Antoine Besnier <besnier_antoine@yahoo.fr>
2025-11-27 09:42:11 +0100

    Initial commit

---
+++ scripts/password.php
@@ -0,0 +1,6 @@
+<?php
+
+//Simple security, find the sha256 hash of your password at https://emn178.github.io/online-tools/sha256.html
+// Here's the hash for 'password'
+
+$password = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8";
